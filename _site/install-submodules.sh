7f9eae911e775d0d3075fc35ec7e4b44f13d78e0
Yarianna Tineo <47702016+yariannatineo@users.noreply.github.com>
2024-07-17 12:38:01 -0600

    reverting jekyll

--- _site/install-submodules.sh
+++ _site/install-submodules.sh
@@ -2,9 +2,16 @@ git submodule add https://github.com/BrightcoveLearning/s-scripts
 cd _includes
 git submodule add https://github.com/BrightcoveLearning/s-jekyll-page-includes
 git submodule add https://github.com/BrightcoveLearning/s-support
+git submodule add https://github.com/BrightcoveLearning/s-template-page-includes
 cd ..
 cd assets/images
 git submodule add https://github.com/BrightcoveLearning/s-support-img
 git submodule add https://github.com/BrightcoveLearning/s-feather-img
 cd ..
+git submodule add https://github.com/BrightcoveLearning/s-site-assets-template-stylesheets
+git submodule add https://github.com/BrightcoveLearning/s-site-assets-favicons
+git submodule add https://github.com/BrightcoveLearning/s-site-assets-stylesheets
+git submodule add https://github.com/BrightcoveLearning/s-site-assets-scripts
+git submodule add https://github.com/BrightcoveLearning/s-site-assets-images
+
 git submodule update --init --recursive
